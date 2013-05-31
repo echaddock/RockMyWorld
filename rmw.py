5757d198d8dd4189125fa8af5bc473632787f190
Elizabeth Chaddock <elchaddock@gmail.com>
2013-05-31 02:10:42 -0700

    scscoring algorithm changed

--- rmw.py
+++ rmw.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from flask import Flask
+from flask import g
 from apiclient.discovery import build
 from optparse import OptionParser
 
@@ -19,6 +20,18 @@ FREEBASE_SEARCH_URL = "https://www.googleapis.com/freebase/v1/search?%s"
 from pyechonest import config
 config.ECHO_NEST_API_KEY = "Q1TFFOJJLUTWJ5OPH"
 from pyechonest import artist
+
+def connect_db():
+    return sqlite3.connect('database.db')
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(g, 'db'):
+        g.db.close()
 
 @app.route('/search/')
 def noQueryTerm():
@@ -42,7 +55,7 @@ def noQueryTerm():
 				eventsList.append((e, thisscore))
 	#Sort events
 	sortedList = sorted(eventsList, key = lambda event: event[1], reverse=True)	
-	events = [event for (event, score) in sortedList]	
+	events = [event for (event, score) in sortedList]
 	return json.dumps(events) 
 
 @app.route('/search/<search_term>')
@@ -55,12 +68,15 @@ def search_for_term(search_term):
 		print e
 		return noQueryTerm()
 
-	terms = query_for(a, artistName) #artist name
+	terms = query_for(artistName, artistObject = a) #artist name
 	return search_with_terms(terms)
 
 #Does all the work for both artist and term search
 def search_with_terms(terms):
  	artistProfile = dict() 
+	if(terms is None):
+		return noQueryTerm()
+
 	for genre in terms:
 		artistProfile[genre['name']] = genre['weight']
 	#elif t:
@@ -75,7 +91,8 @@ def search_with_terms(terms):
 
 	#Query SeatGeek for local events
 
-	json_data = urllib.urlopen('http://api.seatgeek.com/2/events?geoip=true&per_page=80').read()
+	json_data = urllib.urlopen('http://api.seatgeek.com/2/events?geoip=' 
+	  'true&per_page=80').read()
 	data = json.loads(json_data)["events"] 
 	eventsList = [] 
 
@@ -95,7 +112,7 @@ def search_with_terms(terms):
 				eventsList.append((e, thisscore))
 
 	#Sort events
-	sortedList = sorted(eventsList, key = lambda event: event[1])	
+	sortedList = sorted(eventsList, key = lambda event: event[1], reverse=True)	
 	events = [event for (event, score) in sortedList]
 	return json.dumps(events) 
 
@@ -111,34 +128,20 @@ def parse_terms(genre_list):
 
 
 #Calculate similarity score for this artist
-def get_score(artistID, baseArtist, artistName):
- 	print "get score"
- 	try:
-		a = artist.Artist('seatgeek:artist:%s' % artistID)
-	except Exception as e:
-	 	print e
-	 	print 'Artist not found'
-	 	return 1000 # bad entry
-
-	score = 0
+def get_score(aID, baseArtist, artistName):
+ 	score = 0
 
 	try:
-		terms = query_for(a, artistName) 
+		terms = query_for(artistName, artistID = aID) 
 	except:
 	 	print 'terms fail'
-		return 1000 # bad 
+		return score # bad 
+	if(terms is None):
+	 	return score
 
 	for genre in terms:
 		if genre['name'] in baseArtist.keys():
-			score += abs(genre['weight'] - baseArtist[genre['name']])
-		else:
-			score += genre['weight'] 
-	
-	#Look for genres in base artist that are not in this artist
-	for genreName in baseArtist.keys():
-		if(genreName not in terms):
-	 		score += baseArtist[genreName]
-	print "return"
+			score += min(genre['weight'], baseArtist[genre['name']])
 	return score
 
 
@@ -182,33 +185,36 @@ def youtube_search(term):
 
 
 #query database or EchoNest API for an artist's terms
-def query_for(artistObject, artistName):
-
- 	#connect to database
-	conn = sqlite3.connect('database.db')
-	cur = conn.cursor()
-
+def query_for(artistName, artistID=None, artistObject=None):
 	#find the artist in artist table
-	cur.execute("SELECT artistID FROM artists WHERE name=:t", {"t": artistName})
-	conn.commit()
+	cur=g.db.execute("SELECT artistID FROM artists WHERE name=:t", {"t": artistName})
+
 	fetch = cur.fetchone()
 	if(fetch):
 		artistID = fetch[0]
 	else:
-	 	#Get terms from echonest if artist not in database
+	 	if(artistObject is None):
+		 
+	 		#Get terms from echonest if artist not in database
+			try:
+				artistObject = artist.Artist('seatgeek:artist:%s' % artistID)
+			except Exception as e:
+		 		print e
+		 		print 'Artist not found'
+	 			return None 
+
 		termsAndWeights = artistObject.get_terms()
+		print 'a'
 		add_to_db(artistName, termsAndWeights)
 		return termsAndWeights 
 
 	try:
-		cur.execute("SELECT termName, weight FROM terms, link WHERE \n\
+		cur=g.db.execute("SELECT termName, weight FROM terms, link WHERE \n\
 		  (link.termID = terms.termID AND link.artistID=:a)", {"a": artistID})
 	except Exception as e:
 	 	print e
 
-	conn.commit()
 	termsAndWeights = cur.fetchall()
-	cur.close()
 
 	#Query Echonest if not in DB
 	try:
@@ -222,11 +228,11 @@ def query_for(artistObject, artistName):
 				reformat.append(subdict)
 			return reformat 
 		else:
+			artistObject = artist.Artist('seatgeek:artist:%s' % artistID)
 			termsAndWeights = artistObject.get_terms()
-			add_to_db(artistName, termsAndWeights)
 	except Exception as e:
 	 	print e
-		return null
+		return None 
 	return termsAndWeights
 
 
@@ -240,33 +246,36 @@ def term_gen(terms):
 
 #Add artist -> terms mappings to database
 def add_to_db(name, terms):
-	conn = sqlite3.connect('database.db')
-	cur = conn.cursor()
 
 	#Add each artist to the artists table
-	cur.execute('insert into artists (name) values (?)', (name,))
-	artistID = cur.lastrowid;
+	try:
+		cur=g.db.execute("select artistID from artists where name \n\
+			=:n", {'n': name})
+		artistID = cur.fetchone()
+		if(not artistID):
+			cur = g.db.execute('insert into artists (name) values (?)', (name,))  
+			artistID = cur.lastrowid
+		else:
+	 		artistID = termID[0]
 
-	#Add each term's name to the terms table
-	for term in terms:
-		try:
-			cur.execute("select termID from terms where termName \n\
+		#Add each term's name to the terms table
+		for term in terms:
+			cur = g.db.execute("select termID from terms where termName \n\
 			  =:n", {'n': term['name']})
 			termID = cur.fetchone()
 			if(not termID):
-				cur.execute('insert into terms (termName) values (?)', (term['name'],))  
+				cur = g.db.execute('insert into terms (termName) values (?)', (term['name'],))  
 				termID = cur.lastrowid
 		 	else:
 		 		termID = termID[0]
 
 			#Add link
-			cur.execute("insert into link (termID, artistID, weight) \n\
+			cur = g.db.execute("insert into link (termID, artistID, weight) \n\
 			  values (?, ?, ?)", (termID, artistID, term['weight']))
-		except Exception as e:
-		 	print e
+	except Exception as e:
+	 	print e
 
 	conn.commit()
-	cur.close()
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
